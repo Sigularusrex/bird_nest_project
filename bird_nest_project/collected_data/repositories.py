dc60abfc01cd5043f1f24160e9c0390566fab0d3
David Sigley <davidsigley@davids-mbp.home>
2024-05-04 18:01:13 +0200

    finalise ingress

--- bird_nest_project/collected_data/repositories.py
+++ bird_nest_project/collected_data/repositories.py
@@ -3,8 +3,14 @@ from bird_nest_project.collected_data.serializers import PlottedDataSerializer
 
 
 class PlottedDataRepository:
-    @classmethod
     def seed(cls, seed_data: list[dict]) -> list[PlottedData]:
-        serializer = PlottedDataSerializer(data=seed_data, many=True)
+        existing_file_paths = PlottedData.objects.values_list('file_path', flat=True)
+        new_data = [data for data in seed_data if data['file_path'] not in existing_file_paths]
+        
+        if not new_data:
+            return []  # If no new data to save, return empty list
+        
+        serializer = PlottedDataSerializer(data=new_data, many=True)
         serializer.is_valid(raise_exception=True)  # Validate the data
         serializer.save()
+        return serializer.instance
